8c8ec8b5db1717d808a67248cf6be1f094408786
Mauricio Leguizamon <leguizamonmauricio@gmail.com>
2025-11-14 01:35:06 -0300

    Country page activity

---
+++ scripts/place.js
@@ -0,0 +1,44 @@
+
+const year = document.querySelector("#currentyear")
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+async function getWeather() {
+    const apiKey = "e4074a526cfb3b6535f34c46f51e2313"
+    const units = "metric"
+    const city = "Seoul"
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const temperature = data.main.temp;
+        const wind = data.wind.speed * 3.6;
+        
+        if (data.cod != 200) {
+            document.getElementById("weather").innerHTML = `<p> City not found!</p>`;
+            return;
+        }
+        
+        function calculateWindChill(tempC, windKmH) {
+            return (13.12 + 0.6215 * tempC - 11.37 * (windKmH ** 0.16) + 0.3965 * tempC * (windKmH ** 0.16)).toFixed(2) + "°C";
+        }
+        let chillDisplay = "N/A";
+        if (wind > 4.8 && temperature <= 10) {
+            chillDisplay = calculateWindChill(temperature, wind);
+        }
+        document.getElementById("weather").innerHTML = `
+            <h2 class="weather-title">Weather <span class="icon-wrapper"><img src="images/weather_icon.svg" alt="Weather icon"></span></h2>
+            <p>Temperature: ${data.main.temp}°C</p>
+            <p>Condition: ${data.weather[0].description}</p>
+            <p>Wind: ${wind.toFixed(2)}km/h</p>
+            <p>Windchill: ${chillDisplay}`;
+    }
+    catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+getWeather();
