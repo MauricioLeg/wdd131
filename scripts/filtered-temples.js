4f00158a8d70df44065f8a589d9d5ea82d9ad260
Mauricio Leguizamon <leguizamonmauricio@gmail.com>
2025-11-22 02:39:22 -0300

    images made local

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -19,7 +19,7 @@ const temples = [
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    "images/aba-nigeria-temple.jpg"
   },
   {
     templeName: "Manti Utah",
@@ -27,7 +27,7 @@ const temples = [
     dedicated: "1888, May, 21",
     area: 74792,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    "images/manti-temple.jpg"
   },
   {
     templeName: "Payson Utah",
@@ -35,7 +35,7 @@ const temples = [
     dedicated: "2015, June, 7",
     area: 96630,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    "images/payson-utah-temple.jpg"
   },
   {
     templeName: "Yigo Guam",
@@ -43,7 +43,7 @@ const temples = [
     dedicated: "2020, May, 2",
     area: 6861,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    "images/yigo-guam-temple.webp"
   },
   {
     templeName: "Washington D.C.",
@@ -51,7 +51,7 @@ const temples = [
     dedicated: "1974, November, 19",
     area: 156558,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    "images/washington-d.c.-temple.jpg"
   },
   {
     templeName: "Lima Perú",
@@ -59,7 +59,7 @@ const temples = [
     dedicated: "1986, January, 10",
     area: 9600,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    "images/lima-peru-temple.jpg"
   },
   {
     templeName: "Mexico City Mexico",
@@ -67,7 +67,7 @@ const temples = [
     dedicated: "1983, December, 2",
     area: 116642,
     imageUrl:
-    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    "images/mexico-city-temple.jpg"
   },
   {
     templeName: "Buenos Aires Argentina",
@@ -75,7 +75,7 @@ const temples = [
     dedicated: "1986, January, 17",
     area: 30659,
     imageUrl:
-	"https://churchofjesuschristtemples.org/assets/img/temples/buenos-aires-argentina-temple/buenos-aires-argentina-temple-4087-main.jpg"
+	"images/bsas-argentina-temple.jpg"
   },
   {
     templeName: "Salt Lake",
@@ -83,7 +83,7 @@ const temples = [
     dedicated: "1893, April, 6",
     area: 382207,
     imageUrl:
-    "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-15669-main.jpg"
+    "images/salt-lake-temple.webp"
   },
   {
 	templeName: "Salta Argentina",
@@ -91,7 +91,7 @@ const temples = [
 	dedicated: "2024, June, 16",
 	area: 27000,
 	imageUrl:
-	"https://churchofjesuschristtemples.org/assets/img/temples/salta-argentina-temple/salta-argentina-temple-48241-main.jpg"
+	"images/salta-argentina-temple.jpg"
   },
 ];
 
